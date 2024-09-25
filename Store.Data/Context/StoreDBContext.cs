f0ff714e2587093d848a39002e358849c9a93eb3
esraa <esraa57.ea@gmail.com>
2024-09-25 17:14:03 +0300

    Add project files.

---
+++ Store.Data/Context/StoreDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data
+{
+    public class StoreDBContext:DbContext
+    {
+        
+        public StoreDBContext(DbContextOptions<StoreDBContext> options) : base(options)
+        {
+            
+        }
+
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+
+        
+    }
+}
